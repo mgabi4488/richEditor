41c01f5d68717bea963ec522a736e0e6f197f557
MGabi <mgabi4488@gmail.com>
2020-10-26 21:07:36 +0200

    Added link

--- src/scripts/richEditor.js
+++ src/scripts/richEditor.js
@@ -176,6 +176,31 @@ class richEditor {
 					range.insertNode(newNode);
 				}
 			}
+		},{
+			text: '<i class="fas fa-link"></i>',
+			tag: '',
+			action: () => {
+				// 
+				let selection = window.getSelection();
+				let range;
+				let documentFragment;
+				let newNode = document.createElement('a');
+				// 
+				newNode.setAttribute('href','https://www.google.com/');
+				// 
+				newNode.setAttribute('target','_blank');
+				// 
+				if ( selection.rangeCount ) {
+					// 
+					range = selection.getRangeAt(0);
+					// 
+					documentFragment = range.extractContents();
+					// 
+					newNode.appendChild(documentFragment);
+					//
+					range.insertNode(newNode);
+				}
+			}
 		}];
 		// 
 		this.convert();
